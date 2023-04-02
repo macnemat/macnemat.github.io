c0abbf2c24e2df87ffa056373aa3edd3ab1f7b38
macnemat <macnemat@gmail.com>
2023-04-02 15:47:22 -0400

    Testing out Image Banners

--- MatthewPortfolio/src/App.jsx
+++ MatthewPortfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import imageBanner from './assets/ImageBanner.jpg'
 import viteLogo from '/vite.svg'
 
 import Card from 'react-bootstrap/Card';
@@ -8,7 +9,7 @@ import Card from 'react-bootstrap/Card';
 import NavBar from './Components/NavBar';
 
 import './App.css'
-
+let imageBanner2 = './assets/ImageBanner.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
   return (
     <div className="App">
       <div><NavBar /></div>
+      <div><img src={imageBanner} className="image-banner" alt="Hello There Image Banner"></img></div>
       <div>
         <p>Hi</p>
         <p>Hi</p>
